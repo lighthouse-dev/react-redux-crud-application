b3202b07cb2ccf9c3638ee8e4083d066e1a123af
lighthouse-dev <lighthouse2.dev@gmail.com>
2020-01-26 22:11:52 +0900

    Component propsのサンプルコード作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 function App() {
+  const profiles = [
+    { name: 'Taro', age: 28 },
+    { name: 'Hanako', age: 26 },
+    { name: 'Ichiro' }
+  ];
+
   return (
     <div>
-      <h1>hello, world!</h1>
+      {profiles.map((profile, index) => {
+        return <User key={index} name={profile.name} age={profile.age} />;
+      })}
     </div>
   );
 }
+
+const User = props => {
+  return (
+    <div>
+      Hi, I am {props.name}, and {props.age} years old!
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1
+};
 
 export default App;
